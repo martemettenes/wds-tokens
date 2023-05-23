30ceba5c85f6cdbcea255445989ef15d1eca14f1
martemettenes <martemettenes@users.noreply.github.com>
2023-05-23 08:30:03 +0000

    Commit from GitHub Actions (CI)

--- token-translation/build/compose/StyleDictionaryColor.kt
+++ token-translation/build/compose/StyleDictionaryColor.kt
@@ -1,7 +1,7 @@
 
 
 // Do not edit directly
-// Generated on Tue, 23 May 2023 08:28:41 GMT
+// Generated on Tue, 23 May 2023 08:30:02 GMT
 
 
 
@@ -11,5 +11,19 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.*
 
 object StyleDictionaryColor {
+  val colorGreen10 = Color(0xff102b1a)
+  val colorGreen20 = Color(0xff1e442e)
+  val colorGreen30 = Color(0xff325b43)
+  val colorGreen40 = Color(0xff457659)
+  val colorGreen50 = Color(0xff70a384)
+  val colorGreen60 = Color(0xffa0cbb1)
+  val colorGreen80 = Color(0xffcfefdc)
+  val colorGreen90 = Color(0xffebfff3)
+  val colorNeutral30 = Color(0xff454545)
+  val colorNeutral50 = Color(0xff959595)
+  val colorNeutral70 = Color(0xffdadada)
+  val colorNeutralBlack = Color(0xff000000)
+  val colorNeutralWhite = Color(0xffffffff)
+  val colorPurple10 = Color(0xff301e63)
   val colorPurple20 = Color(0xff493389)
 }
--- token-translation/build/ios-swift/StyleDictionary+Enum.swift
+++ token-translation/build/ios-swift/StyleDictionary+Enum.swift
@@ -4,26 +4,26 @@
 //
 
 // Do not edit directly
-// Generated on Tue, 23 May 2023 08:28:41 GMT
+// Generated on Tue, 23 May 2023 08:30:02 GMT
 
 
 import UIKit
 
 public enum StyleDictionaryEnum {
-    public static let colorGreen10 = #102b1a
-    public static let colorGreen20 = #1e442e
-    public static let colorGreen30 = #325b43
-    public static let colorGreen40 = #457659
-    public static let colorGreen50 = #70a384
-    public static let colorGreen60 = #a0cbb1
-    public static let colorGreen80 = #cfefdc
-    public static let colorGreen90 = #ebfff3
-    public static let colorNeutral30 = #454545
-    public static let colorNeutral50 = #959595
-    public static let colorNeutral70 = #dadada
-    public static let colorNeutralBlack = #000000
-    public static let colorNeutralWhite = #ffffff
-    public static let colorPurple10 = #301e63
+    public static let colorGreen10 = UIColor(red: 0.063, green: 0.169, blue: 0.102, alpha: 1)
+    public static let colorGreen20 = UIColor(red: 0.118, green: 0.267, blue: 0.180, alpha: 1)
+    public static let colorGreen30 = UIColor(red: 0.196, green: 0.357, blue: 0.263, alpha: 1)
+    public static let colorGreen40 = UIColor(red: 0.271, green: 0.463, blue: 0.349, alpha: 1)
+    public static let colorGreen50 = UIColor(red: 0.439, green: 0.639, blue: 0.518, alpha: 1)
+    public static let colorGreen60 = UIColor(red: 0.627, green: 0.796, blue: 0.694, alpha: 1)
+    public static let colorGreen80 = UIColor(red: 0.812, green: 0.937, blue: 0.863, alpha: 1)
+    public static let colorGreen90 = UIColor(red: 0.922, green: 1.000, blue: 0.953, alpha: 1)
+    public static let colorNeutral30 = UIColor(red: 0.271, green: 0.271, blue: 0.271, alpha: 1)
+    public static let colorNeutral50 = UIColor(red: 0.584, green: 0.584, blue: 0.584, alpha: 1)
+    public static let colorNeutral70 = UIColor(red: 0.855, green: 0.855, blue: 0.855, alpha: 1)
+    public static let colorNeutralBlack = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let colorNeutralWhite = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let colorPurple10 = UIColor(red: 0.188, green: 0.118, blue: 0.388, alpha: 1)
     public static let colorPurple20 = UIColor(red: 0.286, green: 0.200, blue: 0.537, alpha: 1)
     public static let tokenSetOrder0 = global
 }
